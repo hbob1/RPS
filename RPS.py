40d00b786a52a6c7904c1501526eefa7228feddc
H Bob <poisonous.m.cow@gmail.com>
2023-07-15 15:17:07 -0700

    Added Movment in X coordinate

--- RPS.py
+++ RPS.py
@@ -1,17 +1,19 @@
 import random
 import pygame
+import time
 
 # Sizing Game
-Nx = int(9)  # Maximum X pos
-Ny = int(9)  # Maximum Y pos
-Ngo = int(25)  # How many Game Objects
+Nx = int(900)  # Maximum X pos
+Ny = int(900)  # Maximum Y pos
+Ngo = int(500)  # How many Game Objects
 
 # Graphic const
 RED = 255, 0, 0
 GREEN = 0, 255, 0
 BLUE = 0, 0, 255
+BLACK = 0, 0, 0
 
-CIRCLE_RADIUS = 10
+CIRCLE_RADIUS = 5
 
 # Initializing the game
 goType = Ngo * [0]
@@ -36,23 +38,38 @@ print(goType)
 
 # Display Game
 while True:
+    time.sleep(0.5)
+
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    window.fill(BLACK)
+
+
     # Draw game objects
     for ii, go in enumerate(goType):
-        position = (100 * goXpos[ii], 100 * goYpos[ii])
+        position = (1 * goXpos[ii], 1 * goYpos[ii])
         if goType[ii] == 1:
             color = RED
         elif goType[ii] == 2:
             color = GREEN
         else:
             color = BLUE
-
         pygame.draw.circle(window, color, position, CIRCLE_RADIUS)
         pygame.display.flip()  # Update the display
+
+    # Moving objects
+    for ii, go in enumerate(goType):
+        goXpos[ii] = goXpos[ii] + random.randint(-1, 3)
+        if goXpos[ii] > Nx:
+            goXpos[ii] = 0
+        if goXpos[ii] < 0:
+            goXpos[ii] = Nx
+
+
     # Test position
     # pygame.draw.circle(window, PCOLOR, (0, 0), CIRCLE_RADIUS)
     # pygame.display.flip()
