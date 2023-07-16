3d63a9eda37504573ed4ba80da12558fa02e5eae
H Bob <poisonous.m.cow@gmail.com>
2023-07-15 19:57:55 -0700

    Made movement smooth for x axis

--- RPS.py
+++ RPS.py
@@ -5,20 +5,22 @@ import time
 # Sizing Game
 Nx = int(900)  # Maximum X pos
 Ny = int(900)  # Maximum Y pos
-Ngo = int(500)  # How many Game Objects
+Ngo = int(100)  # How many Game Objects
 
 # Graphic const
 RED = 255, 0, 0
 GREEN = 0, 255, 0
 BLUE = 0, 0, 255
 BLACK = 0, 0, 0
-
+GREY = 128, 128, 128
 CIRCLE_RADIUS = 5
+
 
 # Initializing the game
 goType = Ngo * [0]
 goXpos = Ngo * [0]
 goYpos = Ngo * [0]
+
 
 # Initializing display
 WIDTH = 1000
@@ -32,13 +34,18 @@ for ii, go in enumerate(goType):
     goXpos[ii] = random.randint(0, Nx)
     goYpos[ii] = random.randint(0, Ny)
 
+# Initiallize old as current
+goXpos_old = goXpos.copy()
+goYpos_old = goYpos.copy()
+
+
 print(goXpos)
 print(goYpos)
 print(goType)
 
 # Display Game
 while True:
-    time.sleep(0.5)
+    time.sleep(0.05)
 
     # Handle events
     for event in pygame.event.get():
@@ -46,11 +53,12 @@ while True:
             pygame.quit()
             exit()
 
-    window.fill(BLACK)
+
 
 
     # Draw game objects
     for ii, go in enumerate(goType):
+        position_old = (1 * goXpos_old[ii], 1 * goYpos_old[ii])
         position = (1 * goXpos[ii], 1 * goYpos[ii])
         if goType[ii] == 1:
             color = RED
@@ -58,11 +66,30 @@ while True:
             color = GREEN
         else:
             color = BLUE
+
+        # erasing circle at old position
+        pygame.draw.circle(window, BLACK, position_old, CIRCLE_RADIUS)
+        pygame.display.update(
+            position_old[0] - CIRCLE_RADIUS,
+            position_old[1] - CIRCLE_RADIUS,
+            CIRCLE_RADIUS * 2,
+            CIRCLE_RADIUS * 2
+        )
+
+        # draw circle at new position
         pygame.draw.circle(window, color, position, CIRCLE_RADIUS)
-        pygame.display.flip()  # Update the display
+        pygame.display.update(
+            position[0] - CIRCLE_RADIUS,
+            position[1] - CIRCLE_RADIUS,
+            CIRCLE_RADIUS * 2,
+            CIRCLE_RADIUS * 2
+        )
+
+
 
     # Moving objects
     for ii, go in enumerate(goType):
+        goXpos_old[ii] = goXpos[ii]
         goXpos[ii] = goXpos[ii] + random.randint(-1, 3)
         if goXpos[ii] > Nx:
             goXpos[ii] = 0
