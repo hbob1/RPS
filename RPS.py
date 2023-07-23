7d1feef1f46409947cc7a5ac9aac4b7a272bf9f5
H Bob <poisonous.m.cow@gmail.com>
2023-07-23 15:33:55 -0700

    Game is now fun, added radius collision radius logic

--- RPS.py
+++ RPS.py
@@ -1,11 +1,12 @@
 import random
 import pygame
+import math
 import time
 
 # Sizing Game
 Nx = int(900)  # Maximum X pos
 Ny = int(900)  # Maximum Y pos
-Ngo = int(100)  # How many Game Objects
+Ngo = int(300)  # How many Game Objects
 
 # Graphic const
 RED = 255, 0, 0
@@ -14,13 +15,24 @@ BLUE = 0, 0, 255
 BLACK = 0, 0, 0
 GREY = 128, 128, 128
 CIRCLE_RADIUS = 5
+COLLISION_THRASHOLD = 10 * CIRCLE_RADIUS
+VELNX = -7
+VELPX = 7
+VELNY = -4
+VELPY = 4
 
+# Game Objects types
+ROCK = 1
+SCISSORS = 3
+PAPER = 2
+DEAD = 10
+
+clock = pygame.time.Clock()
 
 # Initializing the game
 goType = Ngo * [0]
 goXpos = Ngo * [0]
 goYpos = Ngo * [0]
-
 
 # Initializing display
 WIDTH = 1000
@@ -30,7 +42,7 @@ pygame.display.set_caption("Mark Segal-----------RSP")
 
 # populating Game
 for ii, go in enumerate(goType):
-    goType[ii] = random.randint(1, 3)
+    goType[ii] = random.randint(ROCK, SCISSORS)
     goXpos[ii] = random.randint(0, Nx)
     goYpos[ii] = random.randint(0, Ny)
 
@@ -38,34 +50,32 @@ for ii, go in enumerate(goType):
 goXpos_old = goXpos.copy()
 goYpos_old = goYpos.copy()
 
-
 print(goXpos)
 print(goYpos)
 print(goType)
 
 # Display Game
 while True:
-    time.sleep(0.05)
-
+    #time.sleep(0.05)
+    clock.tick(120)
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-
-
-
     # Draw game objects
     for ii, go in enumerate(goType):
         position_old = (1 * goXpos_old[ii], 1 * goYpos_old[ii])
         position = (1 * goXpos[ii], 1 * goYpos[ii])
-        if goType[ii] == 1:
+        if goType[ii] == ROCK:
             color = RED
-        elif goType[ii] == 2:
+        elif goType[ii] == PAPER:
             color = GREEN
-        else:
+        elif goType[ii] == SCISSORS:
             color = BLUE
+        else:
+            color = BLACK
 
         # erasing circle at old position
         pygame.draw.circle(window, BLACK, position_old, CIRCLE_RADIUS)
@@ -85,16 +95,47 @@ while True:
             CIRCLE_RADIUS * 2
         )
 
-
-
     # Moving objects
     for ii, go in enumerate(goType):
         goXpos_old[ii] = goXpos[ii]
-        goXpos[ii] = goXpos[ii] + random.randint(-1, 3)
+        goXpos[ii] = goXpos[ii] + random.randint(VELNX, VELPX)
         if goXpos[ii] > Nx:
             goXpos[ii] = 0
         if goXpos[ii] < 0:
             goXpos[ii] = Nx
+
+        # Applying for y movement
+        goYpos_old[ii] = goYpos[ii]
+        goYpos[ii] = goYpos[ii] + random.randint(VELNY, VELPY)
+        if goYpos[ii] > Ny:
+            goYpos[ii] = 0
+        if goYpos[ii] < 0:
+            goYpos[ii] = Ny
+
+
+        for jj, other in enumerate(goType):
+            # Computing distance
+            dist = math.sqrt((goXpos[jj] - goXpos[ii])**2 + (goYpos[jj] - goYpos[ii])**2)
+            # if goXpos[ii] == goXpos[jj] and goYpos[ii] == goYpos[jj]: # Checking exact position
+            if dist < COLLISION_THRASHOLD:
+                if goType[ii] == ROCK and goType[jj] == PAPER:
+                    goType[ii] = PAPER
+                    print("R <- P at " + str(goXpos[ii]) + "," + str(goYpos[ii]))
+                if goType[ii] == ROCK and goType[jj] == SCISSORS:
+                    goType[jj] = ROCK
+                    print("R -> S at " + str(goXpos[ii]) + "," + str(goYpos[ii]))
+                if goType[ii] == SCISSORS and goType[jj] == PAPER:
+                    goType[jj] = SCISSORS
+                if goType[ii] == SCISSORS and goType[jj] == ROCK:
+                    goType[ii] = ROCK
+                if goType[ii] == PAPER and goType[jj] == SCISSORS:
+                    goType[ii] = SCISSORS
+                if goType[ii] == PAPER and goType[jj] == ROCK:
+                    goType[jj] = PAPER
+
+
+
+
 
 
     # Test position
